0c84a98b2dadd8867c0c22e8d8d15c91b53f3284
Alexis <42919405+dxzander@users.noreply.github.com>
2024-03-05 22:52:49 -0600

    Day 4: messing around with physics and stuff

--- Amorphous.gd
+++ Amorphous.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends CharacterBody3D
 
 @export var SPEED = 10.0
 
@@ -23,12 +23,13 @@ func _on_ready():
 func _physics_process(delta):
 	#rotate(Vector3(0.0, 0.0, 1.0), 0.01)
 	
-	var plane1 = Plane(fl_leg.global_position, fr_leg.global_position, br_leg.global_position)
-	var plane2 = Plane(fr_leg.global_position, fl_leg.global_position, bl_leg.global_position)
-	var avg_normal = ((plane1.normal + plane2.normal) / 2 ).normalized()
-	
-	var target_basis = _basis_from_normal(avg_normal)
-	transform.basis = lerp(transform.basis, target_basis, SPEED * delta).orthonormalized()
+	#var plane1 = Plane(fl_leg.global_position, fr_leg.global_position, br_leg.global_position)
+	#var plane2 = Plane(fr_leg.global_position, fl_leg.global_position, bl_leg.global_position)
+	#var avg_normal = ((plane1.normal + plane2.normal) / 2 ).normalized()
+	#print(upOrientation)
+	#
+	#var target_basis = _basis_from_normal(avg_normal)
+	#transform.basis = lerp(transform.basis, target_basis, SPEED * delta).orthonormalized()
 	
 	_handle_movement(delta)
 	pass
@@ -42,19 +43,19 @@ func _handle_movement(delta):
 	
 	var x_dir = Input.get_axis("ui_right", "ui_left")
 	var y_dir = Input.get_axis("ui_up", "ui_down")
-	#var direction = ($Camera.global_transform.basis * Vector3(x_dir, 0, y_dir)).normalized()
-	var direction = (Vector3(x_dir, 0, y_dir)).normalized()
-	translate(direction * SPEED * delta)
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-	#
-	#move_and_slide()
+	var direction = ($Camera.global_transform.basis * Vector3(x_dir, 0, y_dir)).normalized()
+	#var direction = (Vector3(x_dir, 0, y_dir)).normalized()
+	#translate_object_local(direction * SPEED * delta)
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	rotate_object_local(Vector3.UP, x_dir * rotation_speed * delta)
+	move_and_slide()
+	
+	rotate_object_local(get_up(), x_dir * rotation_speed * delta)
 
 func _basis_from_normal(normal: Vector3) -> Basis:
 	var result = Basis()
