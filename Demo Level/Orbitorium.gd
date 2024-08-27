b294cbd16f6259d6d630cb26371823dd7e02c976
Alexis <42919405+dxzander@users.noreply.github.com>
2024-08-26 21:53:40 -0600

    torus train now works, more cleanups and stuff

--- Demo Level/Orbitorium.gd
+++ Demo Level/Orbitorium.gd
@@ -1,10 +1,5 @@
 extends AnimatableBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	rotate(Vector3(1.0, 0.0, 0.0), 0.5 * delta)
 	pass
--- Demo Level/torus_contents.gd
+++ Demo Level/torus_contents.gd
@@ -1,12 +1,5 @@
-extends Node3D
+extends AnimatableBody3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	rotate(Vector3(0.0, 1.0, 0.0), 0.4 * delta)
 	pass
